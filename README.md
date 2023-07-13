28032eb8f855b2114536be00bb2d34299b1cb320
sivakumar-1997 <121223105+sivakumar-1997@users.noreply.github.com>
2023-07-13 18:44:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,104 @@
+# webapp-clojure-2020
+
+## Rationale
+
+I needed project setup to support following workflow:
+
+- The application running in development mode behave like application running in production 
+  (as much as possible). So all development code is addictive and excluded from production build.
+- The application state keep running and reloading automatically on changes.
+- Developer can easily extend development environment with any required tools.
+
+So in about 2 years I've gradually built this prototype for my own needs. 
+
+## Features
+
+### Project
+
+- Leiningen based project.
+- Code style settings for IntelliJ IDEA with Cursive.
+
+### System
+
+- Integrant for application and development systems.
+- Parallel start of integrant components.
+- Separate sources for application and development code.
+- Hot reloading on source files changes.
+- `mount` as integrant component for compiled dependencies in code.
+- Configuration in JAVA properties files.
+- Daemon interface to be run as service with `jsvc`.  
+
+### HTTP Server
+
+- `Immutant` web server with multiple webapps, single port, multiple hostnames.
+- Routing: `metosin/reitit`.
+- Page-rendering: `hiccup`.
+
+### Frontend
+
+- ClojureScript with Shadow CLJS (lein integration).
+- React JS + Rum + Server-side rendering (SSR) + Passing component data from server
+- Tailwind CSS
+- Reload pages without Shadow CLJS (adapted `ring-refresh`)
+
+### SQL Database
+
+- `next.jdbc` JDBC wrapper.
+- `HugSQL` “query builder”.
+- `HikariCP` connection pool.
+- Log database queries via `p6spy`.
+- Database migrations with `Liquibase`.
+- Separate read-write and read-only database connections.
+
+## Q&A
+
+### Why mount _and_ integrant?
+
+During migration of my setup from mount to integrant I found:
+- that integrant is good for 
+  - managing “big” components like web server, database connection pool and so on, 
+    which don't require direct reference in the code,
+  - managing multiple systems like application and development once. 
+- but passing integrant state around as map to access it from code is
+  - annoying,
+  - not so performant as mount,
+  - harder to trace dependencies in code using IDE's navigation tools.
+  
+So I decided to take best from both worlds and use mount and integrant simultaneously.   
+
+## Installation
+
+1. OpenJDK 11 https://adoptopenjdk.net/
+2. Leiningen https://leiningen.org/
+3. Node.js https://nodejs.org/
+4. npm modules: `npm install --no-package-lock`
+
+## Usage
+
+Run for development:
+
+    lein run
+
+Run to check build with release options:
+
+    lein clean
+    lein with-profile test-release run
+
+Build release:
+
+    lein uberjar
+    
+Run built release:
+
+    java -Dconfig.file=dev/app/config/default.props -jar ./target/uberjar/website.jar
+
+## Configuration
+
+Custom configuration properties can be placed in optional file (excluded from version control):
+
+    dev/app/config/user.props
+
+Custom configuration for the development environment can be placed in the optional file
+(excluded from version control):
+
+    dev/dev/config/user.edn
---
+++ project.clj
@@ -0,0 +1,105 @@
+(defproject name.trofimov/webapp-clojure-2020 "1.0.0-SNAPSHOT"
+  :description "Multi-page web application prototype with Clojure(Script)"
+  :dependencies [;;; Clojure
+                 [org.clojure/clojure "1.11.0"]
+
+                 ;;; ClojureScript (shadow-cljs)
+                 [com.google.guava/guava "31.1-jre" :scope "provided"]
+                 [thheller/shadow-cljs "2.17.8" :scope "provided"]
+
+                 ;;; System
+                 [integrant "0.8.0"]
+                 [mount "0.1.16"]
+                 [tolitius/mount-up "0.1.3"]
+
+                 ;;; Web Server
+                 [io.undertow/undertow-core,,,,,,,,,, "2.2.16.Final"]
+                 [io.undertow/undertow-servlet,,,,,,, "2.2.16.Final"]
+                 [io.undertow/undertow-websockets-jsr "2.2.16.Final"]
+                 [metosin/reitit-core "0.5.17"]
+                 [org.immutant/web "2.1.10"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-defaults "0.3.3"]
+
+                 ;;; Database
+                 [com.h2database/h2 "2.1.210"]
+                 [com.layerware/hugsql "0.5.1" :exclusions [com.layerware/hugsql-adapter-clojure-java-jdbc]]
+                 [com.layerware/hugsql-adapter-next-jdbc "0.5.1"]
+                 [com.mattbertolini/liquibase-slf4j "4.0.0"]
+                 [com.zaxxer/HikariCP "5.0.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.liquibase/liquibase-core "4.9.0"]
+                 [seancorfield/next.jdbc "1.2.659"]
+                 [p6spy/p6spy "3.9.1"]
+
+                 ;;; Logging
+                 [ch.qos.logback/logback-classic "1.2.11"]
+                 [ch.qos.logback/logback-core "1.2.11"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.codehaus.janino/janino "3.1.6"]
+                 [org.slf4j/jul-to-slf4j "1.7.36"]
+                 [org.slf4j/slf4j-api "1.7.36"]
+
+                 ;;; Libs (Java)
+                 [com.fasterxml.jackson.core/jackson-core "2.13.2"]
+                 [commons-codec/commons-codec "1.15"]
+                 [org.apache.commons/commons-lang3 "3.12.0"]
+
+                 ;;; Libs (Clojure)
+                 [clojurewerkz/propertied "1.3.0"]
+                 [com.cognitect/transit-clj "1.0.329"]
+                 [medley "1.3.0"]
+                 [potemkin "0.4.5"]
+
+                 ;;; Libs (ClojureScript)
+                 [cljsjs/react "18.0.0-rc.0-0"]
+                 [cljsjs/react-dom "18.0.0-rc.0-0"]
+                 [rum "0.12.8"]
+
+                 ;;; Daemon
+                 [commons-daemon/commons-daemon "1.3.0"]]
+
+  :main ^:skip-aot app.main
+  :test-paths ["test" "src"]
+  :target-path "target/%s"
+  :plugins [[lein-shell "0.5.0"]]
+
+  :clean-targets ^{:protect false} ["target"
+                                    "resources/public/app"]
+
+  :repl-options {:init-ns dev.env.main}
+
+  :shell {:commands
+          {"node_modules/.bin/postcss"
+           {:windows "node_modules/.bin/postcss.cmd"}}}
+
+  :aliases {"shadow-cljs" ["run" "-m" "shadow.cljs.devtools.cli"]
+
+            "css-example-release" ["shell"
+                                   "node_modules/.bin/postcss"
+                                   "tailwind/app/\\$_example/main.css"
+                                   "-o" "resources/public/app/example/main.css"
+                                   "--config" "tailwind/app/config/"]}
+
+  :profiles {:dev {:jvm-opts ["-Dconfig.file=dev/app/config/default.props"]
+                   :main ^:skip-aot dev.env.main
+                   :dependencies [[compojure "1.6.2" #_"For ring-refresh"]
+                                  [me.raynes/fs "1.4.6"]
+                                  [nrepl "0.9.0"]
+                                  [ns-tracker "0.4.0"]
+                                  [ring-refresh "0.1.2"]
+                                  [ring/ring-devel "1.9.5"]
+                                  [zcaudate/hara.io.watch "2.8.7"]]
+                   :source-paths ["dev" "tailwind"]}
+
+             :test-release [:uberjar
+                            {:jvm-opts ["-Dconfig.file=dev/app/config/default.props"]}]
+
+             :uberjar {:aot :all
+                       :prep-tasks ["compile"
+                                    ["shadow-cljs" "release" "example"]
+                                    "css-example-release"]
+                       :injections [(do (println "Disable clojure.test/*load-tests*")
+                                        (require 'clojure.test)
+                                        (alter-var-root #'clojure.test/*load-tests* (constantly false)))]}}
+
+  :uberjar-name "website.jar")
